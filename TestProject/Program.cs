a3647a2775a999e92cc1b5c198d95df1c0536cd6
ja <ja>
2020-06-02 10:09:25 +0300

    Commit second change

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -7,7 +7,7 @@ namespace TestProject
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-            Console.WriteLine("Creating first change and push it to git - Joseph Akl ");
+           
         }
     }
 }

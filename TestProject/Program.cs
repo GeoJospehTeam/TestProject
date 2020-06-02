69c9c3a97ebc801995e0d77b50405d5de4d3dc47
ja <ja>
2020-06-02 10:07:26 +0300

    Commit first change

---
+++ TestProject/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace TestProject
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.WriteLine("Creating first change and push it to git - Joseph Akl ");
+        }
+    }
+}
